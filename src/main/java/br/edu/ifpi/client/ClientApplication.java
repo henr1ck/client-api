1dfac7e5bab487fdee6afe05fbfad48d1ca0154f
henr1ck <pedrohenrick.dev@gmail.com>
2022-12-07 09:12:37 -0300

    Basic authentication with user in memory

--- src/main/java/br/edu/ifpi/client/ClientApplication.java
+++ src/main/java/br/edu/ifpi/client/ClientApplication.java
@@ -2,6 +2,8 @@ package br.edu.ifpi.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -9,5 +11,6 @@ public class ClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/br/edu/ifpi/client/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package br.edu.ifpi.client.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return security.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(BCryptPasswordEncoder bCryptPasswordEncoder){
+        UserDetails userDetails = User.builder()
+                .username("pedro")
+                .password(bCryptPasswordEncoder.encode("12345"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+}
